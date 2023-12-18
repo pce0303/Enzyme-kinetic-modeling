d761364b6529ea1d65b49e5b04e8838664110c5e
pce0303 <ivychoen@gmail.com>
2023-12-18 10:17:51 +0900

    add drawGraph

--- arg.c
+++ arg.c
@@ -2,6 +2,7 @@
 #include <math.h>
 
 double calcRate(double T, int A, double Ea);
+void drawGraph(double activationEnergy);
 
 int main() {
   double temperature; //온도
@@ -13,11 +14,14 @@ int main() {
   scanf("%d", &collisions);
 
   double Energy; //활성화 에너지
-  printf("활성화 에너지 값을 입력하세요 (kJ/mol): ");
+  printf("활성화 에너지 값을 입력하세요 (20kJ/mol ~ 200kJ/mol): ");
   scanf("%lf", &Energy);
 
+  printf("\n활성화 에너지 그래프 : \n");
+  drawGraph(Energy); //활성화 에너지 그래프
+
   double Rate = calcRate(temperature, collisions, Energy);
-  printf("이 효소의 반응 속도상수는 %e 입니다.\n", Rate);  //반응 속도 출력
+  printf("이 효소의 반응 속도 상수는 %e 입니다.\n", Rate);  //반응 속도 출력
 
   return 0;
 }
@@ -29,4 +33,27 @@ double calcRate(double T, int A, double Ea) {
   // 자연상수 e의 지수제곱 exp 함수
 
   return k;
+}
+
+void drawGraph(double activationEnergy) {
+    int i, j;
+    int maxEnergy = 20;  // 그래프의 최대 높이
+    int width = 20;      // 그래프의 폭
+    double pi = 3.14159;
+
+    // 활성화 에너지 함수
+    for (i = 0; i < width; i++) {
+        double activationFunction = -pow((double)i / width, 2) * activationEnergy;
+
+        // 그래프 출력
+        int height = activationFunction + maxEnergy;
+        for (j = 0; j < height; j++) {
+            if (j == maxEnergy) {
+                printf("|");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("*\n");
+    }
 }
